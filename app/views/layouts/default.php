4b25359a5aca40dca8f20323bd8a2bb4ab52e621
pmalicki11 <pmalicki11@gmail.com>
2020-05-03 13:29:50 +0200

    Menu style added

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -3,20 +3,20 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="<?=PROOT?>css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <title><?=$this->getSiteTitle();?></title>
     <?=$this->content('head');?>
   </head>
   <body>
     <h1>Food Registration</h1>
-    <ul>
-      <li><a href="<?=PROOT?>home/index">Home</a></li>
-      <li><a href="<?=PROOT?>products/index">Products</a></li>
-      <li><a href="<?=PROOT?>products/add">Add product</a></li>
-      <li><a href="<?=PROOT?>components/index">Components</a></li>
-      <li><a href="<?=PROOT?>components/add">Add component</a></li>
+    <ul class="nav-menu" id="nav">
+      <li class="nav-item"><a href="<?=PROOT?>home/index">Home</a></li>
+      <li class="nav-item"><a href="<?=PROOT?>products/index">Products</a></li>
+      <li class="nav-item"><a href="<?=PROOT?>products/add">Add product</a></li>
+      <li class="nav-item"><a href="<?=PROOT?>components/index">Components</a></li>
+      <li class="nav-item"><a href="<?=PROOT?>components/add">Add component</a></li>
     </ul>
     <?=$this->content('body');?>
   </body>
--- app/views/products/index.php
+++ app/views/products/index.php
@@ -1,32 +1,34 @@
 <?php $this->setSiteTitle('Products');?>
 <?php $this->start('body');?>
 <p>Products Page</p>
-<table class="table-list">
-  <tr class="tr-list">
-    <th class="th-list">Name</th>
-    <th class="th-list">Producer</th>
-    <th class="th-list">Portion</th>
-    <th class="th-list">Energy</th>
-    <th class="th-list">Fat</th>
-    <th class="th-list">Carbohydrates</th>
-    <th class="th-list">Protein</th>
-    <th class="th-list"></th>
-  </tr>
-  <?php foreach($this->products as $product): ?>
+<div class="list-container">
+  <table class="table-list">
     <tr class="tr-list">
-      <td class="td-list"><?=$product['name']?></td>
-      <td class="td-list"><?=$product['producer']?></td>
-      <td class="td-list"><?=$product['portion']?></td>
-      <td class="td-list"><?=$product['energy']?></td>
-      <td class="td-list"><?=$product['fat']?></td>
-      <td class="td-list"><?=$product['carbohydrates']?></td>
-      <td class="td-list"><?=$product['protein']?></td>
-      <td class="td-list">
-        <a href="<?=PROOT?>products/delete/<?=$product['id']?>" onclick="if(!confirm('Are you sure?')){return false;}">
-          <button class="btn-remove"><i class="material-icons color-red">clear</i> Remove</button>
-        </a>
-      </td>
+      <th class="th-list">Name</th>
+      <th class="th-list">Producer</th>
+      <th class="th-list">Portion</th>
+      <th class="th-list">Energy</th>
+      <th class="th-list">Fat</th>
+      <th class="th-list">Carbohydrates</th>
+      <th class="th-list">Protein</th>
+      <th class="th-list"></th>
     </tr>
-  <?php endforeach; ?>
-</table>
+    <?php foreach($this->products as $product): ?>
+      <tr class="tr-list">
+        <td class="td-list"><?=$product['name']?></td>
+        <td class="td-list"><?=$product['producer']?></td>
+        <td class="td-list"><?=$product['portion']?></td>
+        <td class="td-list"><?=$product['energy']?></td>
+        <td class="td-list"><?=$product['fat']?></td>
+        <td class="td-list"><?=$product['carbohydrates']?></td>
+        <td class="td-list"><?=$product['protein']?></td>
+        <td class="td-list">
+          <a href="<?=PROOT?>products/delete/<?=$product['id']?>" onclick="if(!confirm('Are you sure?')){return false;}">
+            <button class="btn-remove"><i class="material-icons color-red">clear</i> Remove</button>
+          </a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</div>
 <?php $this->end();?>
